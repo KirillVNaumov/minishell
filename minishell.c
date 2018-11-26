59199aa4e856bc0280180064828217f8bdb46df1
Allen Melikian <allenwem@gmail.com>
2018-11-26 13:12:03 -0800

    a bit change

--- minishell.c
+++ minishell.c
@@ -6,11 +6,27 @@
 /*   By: amelikia <amelikia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 13:07:42 by amelikia          #+#    #+#             */
-/*   Updated: 2018/11/26 13:07:43 by amelikia         ###   ########.fr       */
+/*   Updated: 2018/11/26 13:11:32 by amelikia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+void	begin(void)
+{
+	char	*line;
+	int		e_status;
+
+	e_status = 0;
+	while (1 || e_status == 1)
+	{
+		ft_printf("$ >");
+		while (get_next_line(line) > 0)
+		{
+			ft_printf("LOL %s\n", line);
+		}
+	}
+}
+
+int		main(void)
 {
 	begin();
 	return (0);
