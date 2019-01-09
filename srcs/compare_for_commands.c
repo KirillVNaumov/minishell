213b6f6e8150e8e1d30a02659ca1f45771f84eab
Allen Melikian <allenwem@gmail.com>
2019-01-09 13:30:43 -0800

    added tild support

--- srcs/compare_for_commands.c
+++ srcs/compare_for_commands.c
@@ -80,6 +80,31 @@ void	go_to_cd(char **argv)
 	}
 }
 
+char	***find_tild(char ***commands)
+{
+	char	*home;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (commands[i])
+	{
+		j = 1;
+		while (commands[i][j])
+		{
+			if (commands[i][j][0] == '~')
+			{
+				home = find_home();
+				commands[i][j] = ft_strjoin(ft_strjoin("/", home)\
+				, &commands[i][j][1]);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (commands);
+}
+
 void	compare_to_commands(char **commands)
 {
 	int		i;
@@ -87,6 +112,7 @@ void	compare_to_commands(char **commands)
 
 	d_comm = divide_commands(commands);
 	i = 0;
+	d_comm = find_tild(d_comm);
 	while (d_comm && d_comm[i] && d_comm[i][0])
 	{
 		if (check_if_empty(d_comm[i][0]) == 1)
